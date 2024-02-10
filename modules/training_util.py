6116f7aec6fe2db8b19339cb0f59e5a3a4be072c
André F. Gonçalves <andrefg.deh@gmail.com>
2024-02-09 21:37:41 -0300

    Added accuracy by epoch per camera during validation

--- modules/training_util.py
+++ modules/training_util.py
@@ -83,7 +83,10 @@ class PytorchTraining:
             log.writelines('=' * 10+'\n')
             log.close() 
         
-        # codelist = {x:[0,0] for x in self.dataset.codes['test']}
+        # Overall accuracy per camera
+        codelist = {x:[0,0] for x in self.dataset.codes['val']}
+        # Accuracy per epoch
+        code_epoch = []
         
         since = time.time()
         early_stop = False
@@ -96,16 +99,15 @@ class PytorchTraining:
     
         while epoch < num_epochs+1 and not early_stop:
         # for epoch in range(start_epoch, num_epochs+1):
+        
             epoch_start = time.time()
             
-            epoch_info = 'Epoch {}/{}'.format(epoch, num_epochs)
-            
+            epoch_info = '\nEpoch {}/{}\n----------\n'.format(epoch, num_epochs)
             with open(log_path,'a') as log:
-                log.writelines('\n'+epoch_info+'\n')
-                log.writelines('-' * 10+'\n')
-            
+                log.writelines(epoch_info)
             print(epoch_info)
-            print('-' * 10)
+            
+            temp_dict = {}
             
             # Each epoch has a training and validation phase
             for phase in self.dataset.training_phases:
@@ -118,7 +120,7 @@ class PytorchTraining:
                 running_corrects = 0
 
                 # Iterate over data.
-                for inputs, labels, path in self.dataset.dataloaders[phase]:
+                for inputs, labels, paths in self.dataset.dataloaders[phase]:
                     inputs = inputs.to(self.device)
                     labels = labels.to(self.device)
 
@@ -138,6 +140,15 @@ class PytorchTraining:
                             optimizer.step()
                             
                         else:
+                            # Calculating overall accuracy by camera per epoch
+                            for j, path in enumerate(paths):
+                                code = os.path.basename(path).split(' ')[0]
+                                correct_class = (preds[j] == labels[j]).sum().item() # Correct classification
+                                codelist[code][0] += correct_class 
+                                codelist[code][1] += 1 # Total images evaluated
+                                temp_dict[code] = [codelist[code][0], codelist[code][1]] # maybe rewrite this line, so it doesnt do this for every file with each code
+                            code_epoch.append(temp_dict)
+                                
                             # Append batch prediction results
                             predlist=torch.cat([predlist, preds.view(-1).cpu()])
                             lbllist=torch.cat([lbllist, labels.view(-1).cpu()])
@@ -152,6 +163,7 @@ class PytorchTraining:
                 epoch_loss = running_loss / self.dataset.dataset_sizes[phase]
                 epoch_acc = running_corrects.double() / self.dataset.dataset_sizes[phase]
 
+                # Logging Loss
                 loss_acc_info = '{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc)
                 with open(log_path,'a') as log:
                     log.writelines(loss_acc_info+'\n')      
@@ -169,6 +181,8 @@ class PytorchTraining:
                     best_model_wts = deepcopy(model.state_dict())
             
             epoch_end = time.time() - epoch_start
+            
+            # Logging Epoch info
             epoch_duration_info = 'Epoch duration: {:.0f} m {:.0f}s'.format(epoch_end//60, epoch_end%60)
             lr_info = 'Learning Rate = {}'.format(scheduler.get_last_lr()[0])
             with open(log_path,'a') as log:
@@ -187,6 +201,7 @@ class PytorchTraining:
         
         time_elapsed = time.time() - since
         
+        # Logging complete statistics info
         complete_text = 'Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60)
         print(complete_text)
         best_acc_text = 'Best val Acc: {:4f}'.format(best_acc)
@@ -213,11 +228,22 @@ class PytorchTraining:
             stats.writelines("Confusion matrix")
             for row in conf_mat:
                 stats.writelines("\n{} {}".format(row[0],row[1]))
-            stats.writelines('\nAccuracy per class')
+            stats.writelines('\n\nAccuracy per class')
             cls_acc = '\n'
             for i in class_accuracy:
                 cls_acc += "{:4f} ".format(i)
             stats.writelines(cls_acc)
+            
+            stats.write(f"\n\nOveral accuracy by camera during validation")
+            for key, value in codelist.items():
+                stats.write(f"\n{key}: {(value[0]/value[1]):.2f}")
+                
+            stats.write(f"\n\nCamera accuracy per epoch")
+            for i in range(len(code_epoch)):
+                stats.write(f"\nEpoch {i+1}")
+                for code in self.dataset.codes['val']:
+                    values = code_epoch[i][code]
+                    stats.write(f"\n{code}: {(values[0]/values[1]):.2f}, correctly classifying {values[0]} out of {values[1]} images")
     
         # load best model weights
         model.load_state_dict(best_model_wts)
